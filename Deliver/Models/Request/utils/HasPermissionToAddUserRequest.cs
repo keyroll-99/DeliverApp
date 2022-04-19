17fa24258dacb03b346e306180132a9df538c1bc
Karol Kaźmierczak <kazmierczakkarol1999@gmail.com>
2022-04-19 20:45:02 +0200

    Delete Deliver/Models/Request/utils directory

--- Deliver/Models/Request/utils/HasPermissionToAddUserRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Models.Request.Utils;
-
-public class HasPermissionToAddUserRequest
-{
-    public LoggedUser LoggedUser { get; set; }
-    public Db.Company LoggedUserCompany { get; set; }
-    public Guid TargetCompanyHash { get; set; }
-}
