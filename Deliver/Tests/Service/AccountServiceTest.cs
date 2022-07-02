79fa4aa21d21d409abfd4c598f8bcc90fc1ae6f6
Karol Kaźmierczak <kazmierczakkarol1999@gmail.com>
2022-07-02 15:26:08 +0200

    add backend tests

--- Deliver/Tests/Service/AccountServiceTest.cs
+++ Deliver/Tests/Service/AccountServiceTest.cs
@@ -6,6 +6,7 @@ using Models;
 using Models.Db;
 using Models.Db.ConstValues;
 using Models.Exceptions;
+using Models.Integration;
 using Models.Request.Account;
 using NSubstitute;
 using Repository.Repository.Interface;
@@ -29,6 +30,7 @@ public class AccountServiceTest
             {
                 Id = 1,
                 Username = "test",
+                Email = "email@example.com",
                 Password = BCrypt.Net.BCrypt.HashPassword("test"),
                 Name = "TestName",
                 Surname = "TestSurname",
@@ -41,6 +43,14 @@ public class AccountServiceTest
                         {
                             Name = SystemRoles.Admin
                         }
+                    }
+                },
+                PasswordRecoveries = new List<PasswordRecovery>
+                {
+                    new PasswordRecovery
+                    {
+                        CreateTime = DateTime.Now,
+                        Hash = Guid.Parse("4a423fc8-4971-4d58-94dc-0045d010ff02"),
                     }
                 }
             },
@@ -69,6 +79,22 @@ public class AccountServiceTest
             }
         }.BuildMock();
 
+    private readonly IQueryable<PasswordRecovery> _passwordRecoveryDataMock = new List<PasswordRecovery>
+    {
+        new PasswordRecovery
+        {
+            Id = 1,
+            CreateTime = DateTime.Now.AddMinutes(-60),
+            Hash = Guid.NewGuid()
+        },
+        new PasswordRecovery
+        {
+            Id = 2,
+            CreateTime = DateTime.Now.AddMinutes(-10),
+            Hash = Guid.NewGuid()
+        },
+    }.BuildMock();
+
     #endregion
 
     private readonly IAccountService _service;
@@ -81,9 +107,10 @@ public class AccountServiceTest
     {
         _userRepository = Substitute.For<IUserRepository>();
         _userRepository.GetAll().Returns(_userDataMock);
-        
+
         _passwordRecoveryRepository = Substitute.For<IPasswordRecoveryRepository>();
-        
+        _passwordRecoveryRepository.GetAll().Returns(_passwordRecoveryDataMock);
+
         _loggedUserOptions = Substitute.For<IOptions<LoggedUser>>();
         _loggedUserOptions.Value.Returns(new LoggedUser
         {
@@ -147,4 +174,104 @@ public class AccountServiceTest
         await _userRepository.Received(1).UpdateAsync(Arg.Is<User>(x => x.Password != null));
     }
 
+    [Theory]
+    [InlineData("", "", ErrorMessage.InvalidData)]
+    [InlineData("test", "test", ErrorMessage.UserDosentExists)]
+    public async Task InitRecoveryPassword_WhenRequestIsInvalid_ThrowException(string email, string username, string expectError)
+    {
+        // arrange
+        var request = new PasswordRecoveryInitRequest
+        {
+            Email = email,
+            Username = username,
+        };
+
+        // act
+        Func<Task> act = async () => await _service.InitRecoveryPassword(request);
+
+        // assert
+        await act.Should().ThrowAsync<AppException>().WithMessage(expectError);
+    }
+
+    [Fact]
+    public async Task InitPasswordRecovery_WhenRequestIsValid_ThenAddnRecoveryLinkDoDbAndSendEmail()
+    {
+        // arrange
+        var request = new PasswordRecoveryInitRequest
+        {
+            Username = "test",
+            Email = "email@example.com"
+        };
+
+        // act
+        await _service.InitRecoveryPassword(request);
+
+        // assert
+        await _passwordRecoveryRepository.Received(1).AddAsync(Arg.Is<PasswordRecovery>(x => x.UserId == 1));
+        await _mailService.Received(1).SendPasswordRecoveryMessage(Arg.Is<PasswordRecoveryMessageModel>(x => x.Email == request.Email));
+    }
+
+    [Theory]
+    [InlineData("", "", ErrorMessage.InvalidData)]
+    [InlineData("password", "ss", ErrorMessage.TokenExpiredOrInvalidPasswordRecoveryLink)]
+    public async Task SetNewPassword_WhenReqestIsInvalid_ThenThrowError(string newPassword, string recoveryKey, string expectError)
+    {
+        // arrange
+        var request = new PasswordRecoverySetNewPasswordRequest
+        {
+            NewPassword = newPassword,
+            RecoveryKey = recoveryKey,
+        };
+
+        // act
+        Func<Task> act = async () => await _service.SetNewPassword(request);
+
+        // assert
+        await act.Should().ThrowAsync<AppException>().WithMessage(expectError);
+    }
+
+    [Fact]
+    public async Task SetNewPassword_WhenReqestIsValid_SetNewPassowrd()
+    {
+        // arrange
+        var request = new PasswordRecoverySetNewPasswordRequest
+        {
+            NewPassword = "password",
+            RecoveryKey = "4a423fc8-4971-4d58-94dc-0045d010ff02"
+        };
+
+        // act
+        await _service.SetNewPassword(request);
+
+        // assert
+        await _userRepository.Received(1).UpdateAsync(Arg.Any<User>());
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(3)]
+    public async Task IsValidRecoveryKey_WhenRecoveryKeyIsInvalid_ThenRetrunFalse(int passwordRecoveryId)
+    {
+        // arrange
+        var recoveryKey = _passwordRecoveryDataMock.FirstOrDefault(x => x.Id == passwordRecoveryId);
+
+        // act
+        var result = await _service.IsValidRecoveryKey(recoveryKey?.ToString() ?? Guid.NewGuid().ToString());
+
+        // assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public async Task IsValidRecoveryKey_WhenRecoveryKeyIsValid_ThenReturnTrue()
+    {
+        // arrange
+        var recoveryKey = _passwordRecoveryDataMock.Where(x => x.CreateTime > DateTime.Now.AddMinutes(-30)).First();
+
+        // act
+        var result = await _service.IsValidRecoveryKey(recoveryKey.Hash.ToString());
+
+        // assert
+        result.Should().BeTrue();
+    }
 }
