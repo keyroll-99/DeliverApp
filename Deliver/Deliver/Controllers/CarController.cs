56a9c7d510eb4ede89858878b9ef4cedb691f681
Karol Kaźmierczak <kazmierczakkarol1999@gmail.com>
2022-07-02 20:56:41 +0200

    Test commit

---
+++ Deliver/Deliver/Controllers/CarController.cs
@@ -0,0 +1,12 @@
+﻿using Deliver.CustomAttribute;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Deliver.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class CarController : ControllerBase
+    {
+    }
+}
